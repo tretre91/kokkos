f2f5306475d9e190da2691991472dac34eecee3c
Trévis Morvany <trevismorvany@gmail.com>
2026-09-02 22:42:08 +0200

    [WIP] First try at MDTeamPolicy

--- core/unit_test/default/TestDefaultDeviceDevelop.cpp
+++ core/unit_test/default/TestDefaultDeviceDevelop.cpp
@@ -4,6 +4,7 @@
 #include <gtest/gtest.h>
 
 #include <Kokkos_Macros.hpp>
+#include "impl/Kokkos_AnalyzePolicy.hpp"
 #ifdef KOKKOS_ENABLE_EXPERIMENTAL_CXX20_MODULES
 import kokkos.core;
 #else
@@ -14,6 +15,14 @@ import kokkos.core;
 
 namespace Test {
 
-TEST(defaultdevicetype, development_test) {}
+TEST(defaultdevicetype, development_test) {
+  Kokkos::TeamPolicy<Kokkos::Rank<3>, Kokkos::HIP> policy(
+      Kokkos::HIP(), {4, 2, 3}, Kokkos::AUTO);
+  Kokkos::parallel_for(
+      "test", policy, KOKKOS_LAMBDA(decltype(policy)::member_type team) {
+        auto [x, y, z] = team.league_ranks();
+        Kokkos::printf("%d = (%d,%d,%d)\n", team.league_rank(), x, y, z);
+      });
+}
 
 }  // namespace Test
